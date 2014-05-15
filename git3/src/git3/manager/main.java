9fb68ba64df4d616dbbdfe959b46aaecbf1fcdb1
gubak <gubak@coka.rs>
2014-05-15 14:44:17 +0200

    First commit for git3

---
+++ git3/src/git3/manager/main.java
@@ -0,0 +1,14 @@
+package git3.manager;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("GIT3 START");
+
+		
+		
+		System.out.println("GIT3 STOP");
+
+	}
+
+}
